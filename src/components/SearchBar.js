d0abbafbab76ac7950f265280b374cdba206bf91
Mike <michael.barbone@gmail.com>
2019-08-19 13:04:26 -0400

    Successfully rendering list of images based on user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = {};
+  state = {
+    term: ""
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <label>Image Search</label>
-          <input type="text" placeholder="Car" className="field" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="field"
+            value={this.state.term}
+            onChange={e => this.setState({ term: e.target.value })}
+          />
         </form>
       </div>
     );
